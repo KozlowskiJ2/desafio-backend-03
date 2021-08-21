9f12bd778d34b68106102932c371bc2ed0255cba
Jean Kozlowski <jeankozlowski22@gmail.com>
2021-08-21 00:46:31 -0300

    Cadastrar Produto Pronto

--- back-end/src/controladores/produto/cadastrarProduto.js
+++ back-end/src/controladores/produto/cadastrarProduto.js
@@ -1,18 +1,35 @@
+const conexao = require('../conexao');
+const yup = require('yup');
+const { pt } = require('yup-locales');
+const { setLocale } = require('yup');
+setLocale(pt);
+
 const cadastrarProduto = async (req, res) => {
     const usuario = req.usuario;
-    const {nome, estoque, preco, descricao, categoria, imagem} = req.body;
-  
+    const { nome, estoque, preco, descricao, categoria, imagem } = req.body;
+    const schema = yup.object().shape({
+        nome: yup.string().required(),
+        estoque: yup.number().required(),
+        categoria: yup.string().required(),
+        preco: yup.number().required(),
+        descricao: yup.string(),
+        imagem: yup.string().url()
+    })
+
     try {
-      const cadastro = 'insert into produtos (usuario_id,nome,estoque,categoria,preco,descricao,imagem) values ($1,$2,$3,$4,$5,$6,$7)';
-      const {rowCount} = conexao.query(cadastro, [
-        usuario.id, nome, estoque, categoria, preco, descricao, imagem]);
-  
-      if (rowCount === 0) {
-        return res.status(400).json('Erro ao cadastrar produto!');
-      }
-  
-      res.status(200).json('Produto cadastrado com sucesso!');
+        await schema.validate(req.body);
+        const cadastro = 'insert into produtos (usuario_id,nome,estoque,categoria,preco,descricao,imagem) values ($1,$2,$3,$4,$5,$6,$7)';
+        const { rowCount } = conexao.query(cadastro, [
+            usuario.id, nome, estoque, categoria, preco, descricao, imagem]);
+
+        if (rowCount === 0) {
+            return res.status(400).json('Erro ao cadastrar produto!');
+        }
+
+        res.status(200).json('Produto cadastrado com sucesso!');
     } catch (error) {
-      return res.status(400).json(error.message);
+        return res.status(400).json(error.message);
     }
-  };
+};
+
+module.exports = cadastrarProduto;
